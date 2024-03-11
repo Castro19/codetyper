610f0b0e756e4a869f7c15d08a4d1c03fdf4219b
Castro19 <Castro469698@gmail.com>
2024-03-11 08:20:31 -0700

    A mistyped space, now gets underlined and can be visually seen as an error
    Fixed the styling for the text area to now include error handling for spaces. I did this by adding additional styling for space as a red border bottom to highlight mistyped spaces and also added padding.

--- codetyper-frontend/src/components/modeDisplay/text/TextThis.jsx
+++ codetyper-frontend/src/components/modeDisplay/text/TextThis.jsx
@@ -11,24 +11,44 @@ const TextThis = ({ textToType, text, setText }) => {
       manageTimer(false);
     }
   }, [text]);
-
   return (
     <div className="bg-background text-primaryText p-4 rounded-lg shadow">
       <h2 className="font-mono text-lg">
         {textToType.split("").map((char, index) => {
-          let colorClass =
-            text.length > index
-              ? text.charAt(index) === char
-                ? "text-correctTyping" // Use the text color utility prefix
-                : "text-incorrectTyping" // Use the text color utility prefix
-              : "text-untypedText"; // Use the text color utility prefix
+          // Update the base class to remove the mx-0.5 and apply it conditionally later
+          let baseClass = "transition-colors";
+          let colorClass;
+
+          if (text.length > index) {
+            if (text.charAt(index) === char) {
+              colorClass = "text-correctTyping"; // Correct typing
+            } else {
+              colorClass =
+                char === " " ? "bg-incorrectSpace" : "text-incorrectTyping"; // Special case for spaces
+            }
+          } else {
+            colorClass = "text-untypedText";
+          }
+
+          // Apply a special styling for spaces to make them visible when incorrectly typed
+          let style = {};
+          if (char === " ") {
+            // Adjust style for spaces to ensure visibility
+            style.borderBottom =
+              colorClass === "bg-incorrectSpace" ? "2px solid red" : "none";
+            style.padding = "0 2px"; // This helps to make the space visible without affecting letter spacing
+            baseClass += " mx-0.5"; // Apply horizontal margin only to spaces
+          }
 
           return (
             <span
               key={index}
-              className={`${colorClass} mx-0.5 transition-colors`}
+              className={`${colorClass} ${baseClass}`}
+              style={style}
             >
-              {char}
+              {char === " " && colorClass === "bg-incorrectSpace"
+                ? "\u00A0"
+                : char}
             </span>
           );
         })}
